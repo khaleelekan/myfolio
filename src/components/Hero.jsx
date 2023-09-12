c68b81ac5d90377c93b4ab0bd0150985f0e955ee
khalilullahi abdulraheem <kalilulekan@gmail.com>
2023-09-12 22:14:16 +0100

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,10 +16,10 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I am <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I am <span className='text-[#915EFF]'>Khaleel</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
+            I develop responsive websites, user <br className='sm:block hidden' />
             interfaces and web applications
           </p>
         </div>
@@ -30,7 +30,7 @@ const Hero = () => {
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
+            <motion.dev
               animate={{
                 y: [0, 24, 0],
               }}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         styles.paddingX
       } w-full flex items-center py-5 fixed top-0 z-20 ${
         scrolled ? "y" : "bg-transparent"
-      }`}
+      } bg-black` }
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
